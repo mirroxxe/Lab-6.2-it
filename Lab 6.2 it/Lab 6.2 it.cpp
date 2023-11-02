d35e89ac5ee3fe7fb60d8224d75a71fe4216d41f
Volodya <chuchman002@gmail.com>
2023-11-02 18:00:26 +0200

    Add project files.

---
+++ Lab 6.2 it/Lab 6.2 it.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+
+int countnumbers(const std::vector<int>& a) {
+    int count = 0;
+
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] % 2 != 0) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main() {
+    std::vector<int> a = { 2, 4, 7, 1, 9, 6, 3 };
+
+    int oddCount = countnumbers(a);
+
+    std::cout << "count of numbers: " << oddCount << std::endl;
+
+    return 0;
+}
